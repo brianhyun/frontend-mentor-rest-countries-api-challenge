55eb364f9460971d8f2f87e71020154003c64be2
Brian Hyun <jso.venn@gmail.com>
2023-05-02 20:25:05 -0700

    Fix country info retrieval logic

--- routes/country.js
+++ routes/country.js
@@ -38,19 +38,15 @@ router.get("/country/:code", async (req, res, _) => {
     // Gather all the information for the border countries
     const promises = [];
 
-    for (let i = 0; i < countryObj.borders.length; i++) {
-      let countryCode = countryObj.borders[i];
-
+    for (const borderCountryCode of countryObj.borders) {
       promises.push(
         axios
-          .get("https://restcountries.com/v3.1/alpha/" + countryCode)
+          .get(`https://restcountries.com/v3.1/alpha/${borderCountryCode}`)
           .then(function (response) {
-            const borderCountry = {
-              name: response.data.name,
-              code: response.data.cca2,
-            };
-
-            borderCountries.push(borderCountry);
+            borderCountries.push({
+              name: response.data[0].name.official,
+              code: response.data[0].cca2,
+            });
           })
       );
     }
@@ -67,62 +63,6 @@ router.get("/country/:code", async (req, res, _) => {
         console.error(error);
       });
   }
-
-  //   axios
-  //     .get(countryCodeURL)
-  //     .then(function (response) {
-  //       const countryObj = response.data[0];
-
-  //       // Grab Languages
-  //       const langStr = getLanguages(countryObj);
-
-  //       // Convert Country Codes to Country Names (e.g. FRA -> France)
-  //       const borderCountries = [];
-
-  //       if (!countryObj.hasOwnProperty("borders")) {
-  //         borderCountries.push({ name: "None", code: countryObj.cca2 });
-
-  //         res.render("country", {
-  //           country: countryObj,
-  //           languages: langStr,
-  //           borders: borderCountries,
-  //         });
-  //       } else {
-  //         const promises = [];
-
-  //         for (let i = 0; i < countryObj.borders.length; i++) {
-  //           let countryCode = countryObj.borders[i];
-
-  //           promises.push(
-  //             axios
-  //               .get("https://restcountries.com/v3.1/alpha/" + countryCode)
-  //               .then(function (response) {
-  //                 const borderCountry = {
-  //                   name: response.data.name,
-  //                   code: response.data.cca2,
-  //                 };
-
-  //                 borderCountries.push(borderCountry);
-  //               })
-  //           );
-  //         }
-
-  //         Promise.all(promises)
-  //           .then(function () {
-  //             res.render("country", {
-  //               country: countryObj,
-  //               languages: langStr,
-  //               borders: borderCountries,
-  //             });
-  //           })
-  //           .catch(function (error) {
-  //             console.error(error);
-  //           });
-  //       }
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
 });
 
 module.exports = router;
